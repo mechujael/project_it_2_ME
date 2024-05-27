d9657119ad2a92ada7fbf6f4ac2234c0975012c7
mechujael <61082671+mechujael@users.noreply.github.com>
2024-05-28 01:02:58 +0200

    added collision, one could say

--- main.py
+++ main.py
@@ -20,14 +20,17 @@ class Player(pygame.sprite.Sprite):
     COLOR=(255,0,0)
     def __init__(self,x,y,width,height):
         self.rect=pygame.Rect(x,y,width,height)
+
         self.x_vel=0
         self.y_vel=0
         self.mask=None
         self.direction="left"
+
     
     def move(self,dx,dy):
         self.rect.x+=dx
         self.rect.y+=dy
+        return
     
     def move_left(self,vel):
         self.x_vel=-vel
@@ -38,9 +41,15 @@ class Player(pygame.sprite.Sprite):
         self.x_vel=vel
         if self.direction!="right":
             self.direction="right"
+    
+    def move_up(self,vel):
+        self.y_vel=-vel
+    def move_down(self,vel):
+        self.y_vel=vel
 
     def char_loop(self,fps):
         self.move(self.x_vel, self.y_vel)
+
 
 
 
@@ -68,12 +77,32 @@ def player_move(player):
     if key[pygame.K_RIGHT]:
         player.move_right(PLAYER_VEL)
 
+    if key[pygame.K_UP]:
+        player.move_up(PLAYER_VEL)
+    
+    if key[pygame.K_DOWN]:
+        player.move_down(PLAYER_VEL)
+
+
+
+#collision
+def collision(obj1,obj2,player):
+
+    if pygame.Rect.colliderect(obj1,obj2)==True:
+        player.x_vel=0
+
+        if player.y_vel>0:
+            player.y_vel=-2
+        elif player.y_vel<0:
+            player.y_vel=2
+
 
 #drawing
-def draw(window,player):
+def draw(window,player,square):
 
     background=pygame.image.load(join("assets","bckgrnd","background_1_example.jpeg"))
     window.blit(background,(0,0))
+    pygame.draw.rect(window,(255,0,0),square)
     player.draw(window)
     pygame.display.update()
 
@@ -82,7 +111,7 @@ def main(window):
     clock=pygame.time.Clock()
 
     player = Player(100,100,50,50)
-    
+    square=pygame.Rect(200,200,50,50)
 
 
 
@@ -99,7 +128,8 @@ def main(window):
                 break
         player.char_loop(FPS)
         player_move(player)
-        draw(window,player)
+        draw(window,player,square)
+        collision(player.rect,square,player)
     pygame.quit()
     quit()
 
